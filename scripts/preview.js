d5e15d8cee53c5ff14f0f19bb77b403d965b5f7d
Davi <davifeitosa.dev@protonmail.com>
2021-05-02 21:14:04 -0300

    feat: versionamento iniciado

---
+++ scripts/preview.js
@@ -0,0 +1,38 @@
+let previewContainer = document.getElementById('preview-container');
+function mostrarPreview () {
+  let preview = document.getElementById('preview');
+
+  previewContainer.addEventListener('click', () => {
+    removerImagem(preview)
+    preview.click();
+  })
+}
+
+function lerURL (input) {
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+      /* Caso não exclua o preview antes (se existir), serão adicionadas
+      várias imagens se o usuário clicar para adicionar novamente */
+      let previewExiste = document.getElementById('previewImg');
+      previewExiste && previewExiste.remove();
+
+      let img = document.createElement('img');
+      img.id = 'previewImg';
+      img.className = 'img-fluid';
+      img.setAttribute("src", e.target.result);
+      img.alt = "preview da foto de perfil";
+
+      let cameraIcone = document.getElementById('camera-icone');
+      cameraIcone && cameraIcone.remove(); // se eu deixar o ícone, ele vai atrapalhar a preview
+
+      previewContainer.append(img);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function removerImagem (preview) {
+  preview.value = null;
+}
